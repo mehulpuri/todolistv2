a30189c54f3f5672e944043784888209c397751e
mehulpuri <lonewolfbs1@gmail.com>
2022-02-28 21:55:08 +0530

    added req files

--- app.js
+++ app.js
@@ -151,6 +151,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || '3000';
+app.listen(PORT, function(){
+  console.log("the server is up and running");
 });
